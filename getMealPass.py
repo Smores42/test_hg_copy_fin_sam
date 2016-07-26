472dcaf29bb7004712ade68b7449d506bf6aaed9
Smores42 <samcesario1@gmail.com>
2016-07-26 03:07:37 -0400

    Working Email

--- getMealPass.py
+++ getMealPass.py
@@ -9,6 +9,8 @@ from selenium.webdriver.common.by import By
 
 #IMAP imports
 import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 driver = webdriver.Firefox()
 driver.get("https://www.mealpass.com/")
@@ -48,11 +50,63 @@ try:
 	res = WebDriverWait(driver, 6).until(EC.element_to_be_clickable((By.XPATH,"//*[@id='meal-reserved']/div[2]/div/div[4]/span[2]")))
 	print("Resturant")
 	print(res.text)
+	time = WebDriverWait(driver, 6).until(EC.element_to_be_clickable((By.XPATH,"//*[@id='meal-reserved']/div[2]/div/div[5]/span[2]")))
 except TimeoutException:
 	print("Timeout")
 #Send as an email so it can be inputted into Google
 
-serverName = " "
-sendFrom = " "
-sendTo = " "
-Message = address + res
+msg = MIMEMultipart()
+msg['Subject'] = 'Meal Pass Google Card'
+msg['From'] = 'SamDummy12@gmail.com'
+msg['To']  = "samcesario1@gmail.com"
+#msg.add_header('Content-Type','text/html')
+
+#Mes = time.text + address.text + res.text
+#print(Mes)
+Message1 = "<html><body>Meal Pass Reservation <br />Order for: Sam Cesario<br/>Event:Meal Pass Event"
+Message2 = Message1 + " <br/>When:" + time.text
+Message3 = Message2 + " <br/>Venue:" + res.text
+
+Message5 = Message3 + ("<script type='application/ld+json'>" +
+"{" +
+  "@context': 'http://schema.org',"+
+  "@type': 'EventReservation', "+
+  "reservationNumber': 'E123456789',"+
+  "reservationStatus': 'http://schema.org/Confirmed',"+
+  "underName': {"+
+    "@type': 'Person',"+
+    "name': 'John Smith'"+
+  "},"+
+  "reservationFor': {"+
+    "@type': 'Event',"+
+    "name': 'Foo Fighters Concert',"+
+    "startDate': '2017-03-06T19:30:00-08:00',"+
+    "location': {"+
+      "@type': 'Place',"+
+      "name': 'AT&T Park',"+
+      "address': {"+
+        "@type': 'PostalAddress',"+
+        "streetAddress': '24 Willie Mays Plaza',"+
+        "addressLocality': 'San Francisco',"+
+        "addressRegion': 'CA',"+
+        "postalCode': '94107',"+
+        "addressCountry': 'US'"+
+   "   }"+
+  "  }"+
+ " }"+
+"}"+
+"</script></body></html>")
+
+
+
+msg.attach(MIMEText(Message5,'html'))
+#Connect to SMTP
+smtpO = smtplib.SMTP('smtp.gmail.com:587')
+print("Connected")
+smtpO.ehlo()
+smtpO.starttls()
+smtpO.ehlo()
+smtpO.login('SamDummy12@gmail.com','samgoogle')
+smtpO.sendmail(msg['From'],msg['To'],msg.as_string())
+smtpO.quit()
+
